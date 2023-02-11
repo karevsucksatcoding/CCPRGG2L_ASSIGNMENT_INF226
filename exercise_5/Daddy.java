b09f787c76edefdafc288ff49eb594540ca4c282
Erol Chester Abuan <119905402+karevsucksatcoding@users.noreply.github.com>
2023-02-11 23:00:34 +0800

    Add files via upload

---
+++ exercise_5/Daddy.java
@@ -0,0 +1,8 @@
+public class Daddy {
+    
+    protected String surname = "Abuan";
+
+    protected void showSurname(){
+        System.out.println("My family name is: " + surname);
+    }
+}
---
+++ exercise_5/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    
+    private String name = "erol";
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String personName){
+        this.name=personName;
+    }
+
+}
